20eb7c0efa137be9890cefabd83357351e49f8d7
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-09-08 19:58:57 -0400

    pushing project

---
+++ VirtualAssistant/Controllers/AddNewEntryController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewEntryController.swift
+//  VirtualAssistant
+//
+//  Created by Ibraheem rawlinson on 7/21/19.
+//  Copyright © 2019 Ibraheem rawlinson. All rights reserved.
+//
+
+import UIKit
+
+class AddNewEntryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ VirtualAssistant/Controllers/MapController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualAssistant
+//
+//  Created by Ibraheem rawlinson on 7/21/19.
+//  Copyright © 2019 Ibraheem rawlinson. All rights reserved.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
